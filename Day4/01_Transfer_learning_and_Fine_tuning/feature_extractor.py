a469e3d4fd5ae5cc3f83f145a96ce871ffe7cc86
Jun-Sik Choi <leichoi91@gmail.com>
2017-09-13 16:07:03 +0900

    Upload Transfer learning tutorial code

--- Day4/01_Transfer_learning_and_Fine_tuning/feature_extractor.py
+++ Day4/01_Transfer_learning_and_Fine_tuning/feature_extractor.py
@@ -27,87 +27,91 @@ finetuning과는 다르게 기학습된 모델을 feature extractor로 사용할
 
 '''
 
-'''
-1. Load data
+def feature_extract():
+    '''
+    1. Load data
 
-torchvision 라이브러리의 transform을 통해서 훈련 데이터에 대해서 Crop, augmentation, Normalize를,
-Validation 데이터에 대해서 Crop, Normalize를 편리하게 수행할 수 있습니다.
-'''
+    torchvision 라이브러리의 transform을 통해서 훈련 데이터에 대해서 Crop, augmentation, Normalize를,
+    Validation 데이터에 대해서 Crop, Normalize를 편리하게 수행할 수 있습니다.
+    '''
 
-data_transforms = {
-    'train': transforms.Compose([
-        transforms.RandomSizedCrop(224),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
-    'val': transforms.Compose([
-        transforms.Scale(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
-}
+    data_transforms = {
+        'train': transforms.Compose([
+            transforms.RandomSizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+        'val': transforms.Compose([
+            transforms.Scale(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+    }
 
-# 데이터 경로 및 data loader 설정
-data_dir = 'data'
-dsets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x])
-         for x in ['train', 'val']}
-dset_loaders = {x: torch.utils.data.DataLoader(dsets[x], batch_size=4,
-                                               shuffle=True, num_workers=4)
-                for x in ['train', 'val']}
+    # 데이터 경로 및 data loader 설정
+    data_dir = 'data'
+    dsets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x])
+             for x in ['train', 'val']}
+    dset_loaders = {x: torch.utils.data.DataLoader(dsets[x], batch_size=4,
+                                                   shuffle=True, num_workers=4)
+                    for x in ['train', 'val']}
 
-# 데이터 사이즈와 클래스
-dset_sizes = {x: len(dsets[x]) for x in ['train', 'val']}
+    # 데이터 사이즈와 클래스
+    dset_sizes = {x: len(dsets[x]) for x in ['train', 'val']}
 
-dset_classes = dsets['train'].classes
+    dset_classes = dsets['train'].classes
 
-# GPU 사용 가능 여부
-use_gpu = torch.cuda.is_available()
+    # GPU 사용 가능 여부
+    use_gpu = torch.cuda.is_available()
 
-# Data Loader에서 batch size 만큼의 훈련 데이터 및 클래스 로드
-inputs, classes = next(iter(dset_loaders['train']))
+    # Data Loader에서 batch size 만큼의 훈련 데이터 및 클래스 로드
+    inputs, classes = next(iter(dset_loaders['train']))
 
-# 한 batch 안의 이미지들을 grid로 만들어서 확인해 보기
-out = torchvision.utils.make_grid(inputs)
-imshow(out, title=[dset_classes[x] for x in classes])
+    # 한 batch 안의 이미지들을 grid로 만들어서 확인해 보기
+    out = torchvision.utils.make_grid(inputs)
+    imshow(out, title=[dset_classes[x] for x in classes])
 
-'''
-2. Model Load and Modify
-'''
+    '''
+    2. Model Load and Modify
+    '''
+    # Pretrain된 18 layer residual network를 로드
+    model_conv = torchvision.models.resnet18()
+    model_conv.load_state_dict(torch.load('./data/models/resnet18-5c106cde.pth'))
+    # Load한 모델의 파라미터를 train 과정에서 변경하지 않도록 설정 (requires_grad = False)
+    for param in model_conv.parameters():
+        param.requires_grad = False
 
-model_conv = torchvision.models.resnet18(pretrained=True)
-# Load한 모델의 파라미터를 train 과정에서 변경하지 않도록 설정 (requires_grad = False)
-for param in model_conv.parameters():
-    param.requires_grad = False
+    # 마지막 레이어를 타겟 task에 맞게 수정
+    print("Original Fully connected layer of resnet18 (Last layer):", model_conv.fc)
+    num_ftrs = model_conv.fc.in_features
+    model_conv.fc = nn.Linear(num_ftrs, 2)
+    print("Modified Fully connected layer of resnet18 (Last layer):", model_conv.fc)
 
-# 마지막 레이어를 타겟 task에 맞게 수정
-print("Original Fully connected layer of resnet18 (Last layer):", model_ft.fc)
-num_ftrs = model_conv.fc.in_features
-model_conv.fc = nn.Linear(num_ftrs, 2)
-print("Modified Fully connected layer of resnet18 (Last layer):", model_ft.fc)
+    if use_gpu:
+        model_conv = model_conv.cuda()
 
-if use_gpu:
-    model_conv = model_conv.cuda()
+    # Loss function 설정
+    criterion = nn.CrossEntropyLoss()
 
-# Loss function 설정
-criterion = nn.CrossEntropyLoss()
+    # 모델의 Optimizer 설정
 
-# 모델의 Optimizer 설정
+    optimizer_conv = optim.SGD(model_conv.fc.parameters(), lr=0.001, momentum=0.9)
 
-optimizer_conv = optim.SGD(model_conv.fc.parameters(), lr=0.001, momentum=0.9)
+    '''
+    3. Train & Evaluate
+    '''
 
-'''
-3. Train & Evaluate
-'''
+    model_conv = train_model(model_conv, criterion, optimizer_conv, exp_lr_scheduler,
+                           use_gpu, dset_loaders,dset_sizes,num_epochs=25)
 
-model_conv = train_model(model_conv, criterion, optimizer_conv, exp_lr_scheduler,
-                       use_gpu, dset_loaders,dset_sizes,num_epochs=25)
+    '''
+    4. model visualize
+    '''
+    visualize_model(model_conv, dset_loaders, dset_classes, use_gpu)
 
-'''
-4. model visualize
-'''
-visualize_model(model_conv, dset_loaders, dset_classes, use_gpu)
+    plt.ioff()
+    plt.show()
 
-plt.ioff()
-plt.show()
+    return
